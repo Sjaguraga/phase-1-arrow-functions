a38622e833940ed59baa3984225e7feafcbbf2f2
serref jaguraga <reef163@gmail.com>
2022-02-01 09:49:18 -0500

    commit tree 84b4319c646d4a352b523800bfd2f27eb1d6e54d

---
+++ index.js
@@ -0,0 +1,13 @@
+let num1 = 100
+let num2 = 2000
+const divide = function() {
+    return num2/num1;
+}
+divide();
+const square = ((x) => x * x)
+square();
+
+
+const add = (num3, num4) => num3 + num4
+add(num3, num4);
+
